0a88eba1fb3089a0ad44b822a79b8f0f55588657
Travis CI User <travis@example.org>
2019-05-18 16:04:08 +0000

    Update documentation

---
+++ rubble/phy/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["AdvertisingChannel","One of the three advertising channels (channel indices 37, 38 or 39)."],["ChannelMap","A map marking data channels as used or unused."],["DataChannel","One of 37 data channels on which data channel PDUs are sent between connected devices."]],"trait":[["Radio","Trait for raw 2.4 GHz non-BLE-specific radios."]]});
